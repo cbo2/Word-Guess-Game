6314291488599975cc3f6511b52dd1683fab37d0
Craig Bomba <talk2cbo@gmail.com>
2018-06-12 08:06:50 -0500

    changes since initial start and adding bootstrap

---
+++ assets/javascript/game.js
@@ -0,0 +1,83 @@
+var keysPressedDictionary = { 
+    a: false,
+    b: false,
+    c: false,
+    d: false,
+    e: false,
+    f: false,
+    g: false,
+    h: false,
+    i: false,
+    j: false,
+    k: false,
+    l: false,
+    m: false,
+    n: false,
+    o: false,
+    p: false,
+    q: false,
+    r: false,
+    s: false,
+    t: false,
+    u: false,
+    v: false,
+    w: false,
+    x: false,
+    y: false,
+    z: false
+};
+
+// the phraseList is a tuple of the phrase, the answer, and 3 clues
+var phraseList = [
+    ["Go ahead, make my day", "Sudden Impact - 1983", "Callahan", "Dirty", "Eastwood"],
+    ["May the Force be with you", "Star Wars - 1977", "Han", "Skywalker", "Lucas"],
+    ["E.T. phone home", "E.T.: THE EXTRA-TERRESTRIAL - 1982", "Finger", "Phone", "Elliott"],
+    ["Show me the money!", "JERRY MAGUIRE - 1996", "Football", "Cruise", "Cuba"],
+    ["I'll be back", "THE TERMINATOR - 1984", "", "", ""],
+    ["There's no crying in baseball!", "A LEAGUE OF THEIR OWN - 1992", "", "", ""]
+];
+
+// let's make a random selection for the next phrase
+var randomIndexToPhrase = Math.floor(Math.random() * phraseList.length);
+var currentPhraseAnswer = "";
+var currentPhrase = ""; 
+currentPhrase = currentPhraseAnswer = phraseList[randomIndexToPhrase][0];
+
+// from that selection, get the 3 clues and set them on the carrousel
+document.querySelector("p.clue1").innerHTML = phraseList[randomIndexToPhrase][2];
+document.querySelector("p.clue2").innerHTML = phraseList[randomIndexToPhrase][3];
+document.querySelector("p.clue3").innerHTML = phraseList[randomIndexToPhrase][4];
+
+// Set the inner HTML contents of the .phrase p to our html string
+// But, first, replace all answered letters with underscores and populate puncuation
+currentPhrase.replace(/[a-z]/g, '_');
+// for (i = 0; i < currentPhrase.length; i++) {
+//     if (currentPhrase.charAt(i) >= 'a' && currentPhrase.charAt(i) <= 'z') {
+//         currentPhrase.charAt(i) = '_';
+//     // whether it is a space or a puncuation, leave it as is and do nothing to expose it
+//     }
+// }
+document.querySelector("p.phrase").innerHTML = currentPhrase;
+
+
+document.onkeyup = function(event) {
+
+    // Determines which key was pressed.
+    var userGuess = event.key.toLowerCase();
+
+    // make sure the key pressed was a letter between a - z
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
+        keysPressedDictionary[userGuess] = true;
+        var keysGuessSofar = "";
+        for(var key in keysPressedDictionary) {
+            // if the value in the dictionary was set to true this letter was selected
+            if (keysPressedDictionary[key]) {  
+                keysGuessSofar += key;
+                if (key != "z") {
+                    keysGuessSofar += ", ";
+                } 
+            }
+        }
+        document.querySelector("p.keysGuessed").innerHTML = keysGuessSofar;
+    }
+};
